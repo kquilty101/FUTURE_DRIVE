f0a0ed4723914eb85a4fe3a2d9829d2aa70dbae7
kquilty101 <kquilty10@dgsw.hs.kr>
2022-05-19 18:10:41 +0900

    화면에 벡터값 출력

--- Script/Rotate.cs
+++ Script/Rotate.cs
@@ -6,17 +6,47 @@ public class Rotate : MonoBehaviour
 {
 	Rigidbody rigid;
 	
-    // Start is called before the first frame update
+	private const float MAX_SPEED_ANGLE = -20;
+	private const float ZERO_SPEED_ANGLE = 210;
+	
+	private Transform needleTransform;
+	
+	private float speedMax;
+	private float speed;
+	
+	private GUIStyle guiStyle = new GUIStyle(); //create a new variable
+	
     void Start()
     {
-        
+        //needleTransform = transform.Find("Needle");
+		
+		//speed = 0f;
+		//speedMax = 200f;
     }
 
-    // Update is called once per frame
     void Update()
     {
+		//speed += 30f * Time.deltaTime;
+		//if (speed > speedMax) speed = speedMax;
+		
+		//needleTransform.eulerAngles = new Vector3(0, 0, GetSpeedRotation());
+		
 		rigid = GameObject.Find("CAR").GetComponent<Rigidbody>();
 		Vector3 velocity = rigid.velocity;
-		Debug.Log("velocity = " + velocity);
+		Debug.Log("SPEED = " + velocity);
+		
     }
+	
+	void OnGUI(){
+		Vector3 velocity = rigid.velocity;
+		GUI.Label (new Rect(Screen.width, Screen.height, 300, 50), velocity.ToString());
+		guiStyle.fontSize = 250; //change the font size
+        GUILayout.Label(velocity.ToString(), guiStyle);
+	}
+	
+	/*private float GetSpeedRotation() {
+		float totalAngleSize = ZERO_SPEED_ANGLE - MAX_SPEED_ANGLE;
+		float speedNormalized = speed / speedMax;
+		return ZERO_SPEED_ANGLE - speedNormalized * totalAngleSize;
+	}*/
 }
